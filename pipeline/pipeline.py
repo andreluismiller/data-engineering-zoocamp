f380002324de1f781f46593546fb2e1a1a5c8902
André Miller <46939182+andreluismiller@users.noreply.github.com>
2026-01-12 23:41:03 +0000

    pipeline code working

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,7 +5,7 @@ print('arguments', sys.argv)
 
 month = int(sys.argv[1])
 
-df = pd.Dataframe({"day": [1, 2], "num_passengers": [3, 4]})
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
 print(df.head())
 
